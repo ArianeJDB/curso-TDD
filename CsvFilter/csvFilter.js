3985ea395e4257c10a101632fde8c63946dd46ad
ajurado <ariane.jurado@lifullconnect.com>
2023-05-22 22:42:31 +0200

    when there is IVA and IGIC should remove the line

--- CsvFilter/csvFilter.js
+++ CsvFilter/csvFilter.js
@@ -3,11 +3,31 @@ export const CsvFilter = {
 }
 
 function _create () {
-  function filter () {
-    return ['Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente', '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,']
+  function filter (titles, line) {
+    const lineSplit = line.split(',')
+    if (lineSplit[4] !== '' && lineSplit[5] !== '') {
+      return [titles]
+    }
+    return [titles, line]
   }
 
   return {
     filter
   }
 }
+
+// - Es válido que algunos campos estén vacíos, apareciendo dos comas seguidas o una coma final.
+// - El número de factura no puede estar repetido, si lo estuviese eliminaremos todas las líneas con repetición.
+// - Los impuestos IVA e IGIC son excluyentes, es decir, sólo puede aplicarse uno de los dos.
+//        Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
+// - Los campos CIF y NIF son excluyentes, sólo se puede usar uno de ellos.
+// - El neto es el resultado de aplicar al bruto el correspondiente impuesto.
+//        Si algún neto no está bien calculado la línea se queda fuera.
+
+// -(/) Un fichero con una sola factura donde todo es correcto, debería producir como salida la misma línea
+// - Un fichero con una sola factura donde IVA e IGIC están rellenos, debería eliminar la línea
+// - Un fichero con una sola factura donde el neto está mal calculado, debería ser eliminada
+// - Un fichero con una sola factura donde CIF y NIF están rellenos, debería eliminar la línea
+// - Si el número de factura se repite en varias líneas, se eliminan todas ellas (sin dejar ninguna línea).
+// - Una lista vacía producirá una lista vacía de salida
+// - Un fichero de una sola línea es incorrecto porque no tiene cabecera
--- CsvFilter/tests/csvFilter.test.js
+++ CsvFilter/tests/csvFilter.test.js
@@ -2,14 +2,24 @@ import {CsvFilter} from "../csvFilter.js";
 
 describe('CSV Filter', () => {
     const csvFilter = CsvFilter.create()
-    describe('when there is only recipe in the CSV file', () => {
-        test('returns same line when recipe is correct', async () => {
+    describe('when there is only recipe', () => {
+        test('when recipe is correct returns same line', async () => {
             const titles = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente'
             const line = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,'
 
             const result = csvFilter.filter(titles, line)
 
-            expect(result).toMatchObject([titles, line])
+            expect(result).toStrictEqual([titles, line])
         });
+
+        test('when there is IVA and IGIC should remove the line', async () => {
+            const titles = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente'
+            const line = '1,02/05/2021,1000,790,21,4,ACER Laptop,B76430134,'
+
+            const result = csvFilter.filter(titles, line)
+
+            expect(result).toStrictEqual([titles])
+        });
+
     });
 });
