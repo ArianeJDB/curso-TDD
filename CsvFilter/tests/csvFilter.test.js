86293b7865b615dd3bbe7143b174da0e981ab74f
ajurado <ariane.jurado@lifullconnect.com>
2023-10-14 21:17:16 +0200

    refactor add test each in remove rows case

--- CsvFilter/tests/csvFilter.test.js
+++ CsvFilter/tests/csvFilter.test.js
@@ -11,66 +11,23 @@ describe('CSV Filter', () => {
             expect(result).toStrictEqual([header, rows])
         });
 
-        test('header is empty, returns [] ', async () => {
-            const header = ''
-            const rows = ['1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,N789789']
-
+        test.each([
+            ['header is empty', [], ['1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,N789789']],
+            ['row is empty', 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente', []]
+        ])('when %s returns []', (_, header, rows) => {
             const result = csvFilter.filter({header, rows})
 
             expect(result).toStrictEqual([])
         });
 
-        test('row is empty, returns [] ', async () => {
-            const rows = []
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([])
-        });
-
-        test('there is IVA and IGIC, removes the row', async () => {
-            const rows = ['1,02/05/2021,1000,790,21,4,ACER Laptop,B76430134,']
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([header, []])
-        });
-
-        test('there is not IVA nor IGIC, removes the row ', async () => {
-            const rows = ['1,02/05/2021,1000,790,,,ACER Laptop,B76430134,']
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([header, []])
-        });
-
-        test('IGIC is not a number, removes the row', async () => {
-            const rows = ['1,02/05/2021,1000,790,,er,ACER Laptop,B76430134,']
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([header, []])
-        });
-
-        test('IVA is not a number, removes the row', async () => {
-            const rows = ['1,02/05/2021,1000,790,vhj,,ACER Laptop,B76430134,']
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([header, []])
-        });
-
-        test('neto is calculated wrong, removes the row', async () => {
-            const rows = ['1,02/05/2021,1000,790,,4,ACER Laptop,B76430134,']
-
-            const result = csvFilter.filter({header, rows})
-
-            expect(result).toStrictEqual([header, []])
-        });
-
-        test('there is CIF and NIF, removes the row', async () => {
-            const rows = ['1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,N789789']
-
+        test.each([
+            ['there is IVA and IGIC', ['1,02/05/2021,1000,790,21,4,ACER Laptop,B76430134,']],
+            ['there is not IVA nor IGIC', ['1,02/05/2021,1000,790,,,ACER Laptop,B76430134,']],
+            ['IGIC is not a number', ['1,02/05/2021,1000,790,,er,ACER Laptop,B76430134,']],
+            ['IVA is not a number', ['1,02/05/2021,1000,790,vhj,,ACER Laptop,B76430134,']],
+            ['neto is calculated wrong', ['1,02/05/2021,1000,790,,4,ACER Laptop,B76430134,']],
+            ['there is CIF and NIF', ['1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,N789789']]
+        ])('when %s, removes the row', async (_, rows) => {
             const result = csvFilter.filter({header, rows})
 
             expect(result).toStrictEqual([header, []])
